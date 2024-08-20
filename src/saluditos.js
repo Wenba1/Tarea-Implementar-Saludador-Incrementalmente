0eb73d835b119b9fe25cd1ee8c8b1c8ecbece8ba
Wendy Balderrama <balderramawendym@gmail.com>
2024-08-20 01:03:00 -0400

    Añadi el saludo de genero

--- src/saluditos.js
+++ src/saluditos.js
@@ -2,3 +2,11 @@ export function saludar()
 {
     return "Hola";
 }
+export function saludar(nombre)
+{
+    return 'Hola, $(nombre)'
+}
+export function saludar(nombre, genero){
+    let saludo=genero =='masculino' ? 'Hola, Señor': 'Hola, Señora';
+    return '$(saludo) $(nombre)';
+}
--- src/saluditos.test.js
+++ src/saluditos.test.js
@@ -1,6 +1,16 @@
+// src/index.test.js
 
-import { saludar } from './saluditos';
+import { saludar } from './index';
 
 test('Saludar con "Hola"', () => {
     expect(saludar()).toBe("Hola");
+});
+
+test('Saludar por nombre', () => {
+    expect(saludar("Carlos")).toBe("Hola, Carlos");
+});
+
+test('Saludar por nombre y género', () => {
+    expect(saludar("Chayanne", "masculino")).toBe("Hola, Señor Chayanne");
+    expect(saludar("Maria", "femenino")).toBe("Hola, Señora Maria");
 });
