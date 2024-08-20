1e23fbf79269b0e3065f65b57fe52c204adee4e7
Wendy Balderrama <balderramawendym@gmail.com>
2024-08-20 01:14:23 -0400

    Añadir el saludo en español e ingles

--- src/saluditos.js
+++ src/saluditos.js
@@ -10,3 +10,21 @@ export function saludar(nombre, genero){
     let saludo=genero =='masculino' ? 'Hola, Señor': 'Hola, Señora';
     return '$(saludo) $(nombre)';
 }
+export function saludar(nombre, genero, edad){
+    let saludo= genero ==='masculino' ? 'señor': 'señora';
+    let edadSaludo= edad >50 ? "Mayor":"Joven";
+    return 'Hola, $(saludo) $(nombre), ${edadSaludo})';
+}
+export function saludar(nombre, genero, edad, idioma = 'es') {
+    let saludo = '';
+    if (idioma === 'es') {
+        saludo = genero === 'masculino' ? 'Señor' : 'Señora';
+    } else if (idioma === 'en') {
+        saludo = genero === 'masculino' ? 'Mr.' : 'Mrs.';
+    }
+
+    let edadSaludo = idioma === 'es' ? (edad > 50 ? "Mayor" : "Joven") : (edad > 50 ? "Senior" : "Young");
+    return idioma === 'es' 
+        ? `Hola, ${saludo} ${nombre}, ${edadSaludo}`
+        : `Hello, ${saludo} ${nombre}, ${edadSaludo}`;
+}
--- src/saluditos.test.js
+++ src/saluditos.test.js
@@ -14,3 +14,11 @@ test('Saludar por nombre y género', () => {
     expect(saludar("Chayanne", "masculino")).toBe("Hola, Señor Chayanne");
     expect(saludar("Maria", "femenino")).toBe("Hola, Señora Maria");
 });
+test('Saludar por nombre, género y edad', () => {
+    expect(saludar("Chayanne", "masculino", 55)).toBe("Hola, Señor Chayanne, Mayor");
+    expect(saludar("Maria", "femenino", 30)).toBe("Hola, Señora Maria, Joven");
+});
+test('Saludar en español e inglés', () => {
+    expect(saludar("Chayanne", "masculino", 55, 'es')).toBe("Hola, Señor Chayanne, Mayor");
+    expect(saludar("Maria", "femenino", 30, 'en')).toBe("Hello, Mrs. Maria, Young");
+});
